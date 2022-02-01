bfe77e5c8c264a7242c137f6e8e961f6b20aeab1
unknown <luiscatodd@gmail.com>
2022-02-01 14:38:47 -0600

    Register Route Complete

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import mongoose, { Error } from 'mongoose'
+import mongoose, { Document, Error } from 'mongoose'
 import express from 'express'
 import cors from 'cors'
 import passport from 'passport'
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
+import User from './User'
 
 mongoose.connect("mongodb://localhost:27017/passport-ts", (err: Error) => {
     if (err) throw err
@@ -29,3 +30,35 @@ app.use(
 app.use(cookieParser())
 app.use(passport.initialize())
 app.use(passport.session())
+
+
+//App Routes
+app.post('/register', async (req, res) => {
+
+    const { username, password } = req?.body
+
+    //Check user and password
+    if (!username || !password || typeof username !== "string" || typeof password !== "string") {
+        res.send("Improper Values")
+        return
+    }
+
+    //Check if user exists
+    User.findOne({ username }, async (err: Error, doc: Document) => {
+        if (err) throw err
+        if (doc) res.send("User Already Exists")
+        if (!doc) {
+            const hashPassword = await bcrypt.hash(password, 10)
+            const newUser = new User({
+                username: username,
+                password: hashPassword
+            })
+            await newUser.save()
+            res.send("Success")
+        }
+    })
+})
+
+app.listen(8000, () => {
+    console.log(`Serving on port ${8000}`)
+})
